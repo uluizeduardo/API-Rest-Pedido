a71b730577602124a58077b11c59de6357737753
uluizeduardo <luiz.analistadesistemas@gmail.com>
2022-06-05 21:58:46 -0300

    Projeto Criado

---
+++ src/main/java/com/api/rest/pedido/ApiRestPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.api.rest.pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestPedidoApplication.class, args);
+	}
+
+}
